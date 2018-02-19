bda27de9ed9491f2c7407b7de4023955de7a7d86
Moekr <highter@outlook.com>
2018-02-19 13:24:12 +0800

    Add script exit hook

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+function exit_hook {
+        ps -aux | grep -v grep | grep "python server.py" | awk '{print $2}' | xargs -n 1 kill
+}
+trap exit_hook EXIT
+
 cd `dirname $0`
 ulimit -n 512000
 python server.py
